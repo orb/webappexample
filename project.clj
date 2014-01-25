ee82088f64e7d172dc4e550c12562f97cd714895
Norman Richards <orb@nostacktrace.com>
2014-01-25 17:20:20 -0600

    comment out some things to make it go

--- project.clj
+++ project.clj
@@ -5,17 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
-                 [com.cemerick/piggieback "0.1.2"]
-                 [ring "1.2.1"]  
-                 [compojure "1.1.6" :exclusions [org.clojure/core.incubator]]
+                 #_[org.clojure/clojurescript "0.0-2138"]
+                 #_[com.cemerick/piggieback "0.1.2"]
+                 [ring "1.2.1"]
+                 [compojure "1.1.6"]
                  [enlive "1.1.5"]
                  [domina "1.0.2"]
-                 [com.cemerick/friend "0.2.0"]
-                 [org.clojure/tools.reader "0.8.3"]
-                 [javax.servlet/servlet-api "2.5"]]
-  :plugins [[com.cemerick/austin "0.1.3"]
-            [lein-cljsbuild "1.0.1"]
+                 [com.cemerick/friend "0.2.0"]]
+  :plugins [#_[com.cemerick/austin "0.1.3"]
+            #_[lein-cljsbuild "1.0.1"]
             [lein-ring "0.8.10"]]
 
   :profiles {:dev {:repl-options {:init-ns webappexample.core}
@@ -24,10 +22,11 @@
                                          :compiler {:output-to "resources/public/scripts/app.js"
                                                     :optimizations :simple
                                                     :pretty-print true}}]}}}
-  :main webappexample.core
-  
-  
+  ;;:main webappexample.core
+
+
   :ring {:handler webappexample.core/secured-site
-         :init webappexample.core/-main
-         :listener-class webappexample
+         :context "/"
+         ;;:init webappexample.core/-main
+         ;;:listener-class webappexample
          })
--- src/clj/webappexample/core.clj
+++ src/clj/webappexample/core.clj
@@ -1,11 +1,10 @@
 (ns webappexample.core
-  (:require [cemerick.austin.repls :refer (browser-connected-repl-js)]
+  (:require #_[cemerick.austin.repls :refer (browser-connected-repl-js)]
             [net.cgrand.enlive-html :as html]
             [net.cgrand.reload :as reload]
             [compojure.handler :as handler]
             [compojure.route :refer (resources not-found)]
             [compojure.core :refer (GET POST defroutes)]
-            ring.adapter.jetty
             [ring.util.response :as resp]
             [ring.middleware.params :refer (wrap-params)]
             [ring.middleware.nested-params :refer (wrap-nested-params)]
@@ -18,12 +17,12 @@
             [clojure.set :as set]
             [cemerick.friend :as friend]
             (cemerick.friend [workflows :as workflows]
-                             [credentials :as creds]))
-  (:import java.net.URI)
+                               [credentials :as creds]))
   (:gen-class))
 
-(reload/auto-reload *ns*) ; To automatically reload Enlive templates -
-                          ; wrap-reload used below in handler
+
+;; (reload/auto-reload *ns*) ; To automatically reload Enlive templates -
+;;                           ; wrap-reload used below in handler
 
 ;;; Friend atom and accessor functions
 
@@ -44,8 +43,8 @@
   (:username (second (first (:authentications (:cemerick.friend/identity (:session req)))))))
 
 
-#_(defn get-friend-username [req] ; This doesn't smell right...
-  (:username (second (first (:authentications (:cemerick.friend/identity (:session req)))))))
+;; #_(defn get-friend-username [req] ; This doesn't smell right...
+;;   (:username (second (first (:authentications (:cemerick.friend/identity (:session req)))))))
 
 
 
@@ -72,7 +71,7 @@
 
 (html/defsnippet navbar (io/resource "public/landing.html")
   [:body :div.navbar]
-  [req]  
+  [req]
   [:ul [:li html/first-of-type]] (if (friend/identity req)
                                    ;users see the app item in their menu
                                    (html/clone-for [[caption uri] navigation-items-user]
@@ -93,27 +92,28 @@
 (html/defsnippet non-app-content (io/resource "public/landing.html")
   [:#content]
   [req]
-  [:#content] (case (get-navigation-caption req) 
-                "Home" (html/set-attr :id "content") 
+  [:#content] (case (get-navigation-caption req)
+                "Home" (html/set-attr :id "content")
                 "About" (html/do-> (html/content "See what we're all about")
                                    (html/wrap :h2))
                 "Contact" (html/do-> (html/content "Learn how to make contact")
                                      (html/wrap :h2))
                 (html/do-> (html/content  "Best check yo self, page not found!")
                                    (html/wrap :h2 {:class "alert alert-warning" :style "text-align: center;"}))))
-  
+
 (html/deftemplate landing (io/resource "public/landing.html")
   [req]
   [:body :div.navbar] (html/substitute (navbar req))
   [:body :#content] (html/substitute (non-app-content req))
-  [:body] (html/append (html/html [:script (browser-connected-repl-js)])))
+  ;;[:body] (html/append (html/html [:script (browser-connected-repl-js)]))
+  )
 
-;;; Default page for erroneous logins 
+;;; Default page for erroneous logins
 (html/deftemplate login (io/resource "public/landing.html")
   [req]
   [:body :div.navbar] (html/substitute (navbar req))
   ;[:body :#content] (html/substitute (non-app-content req))
-  [:body] (html/append (html/html [:script (browser-connected-repl-js)]))
+  ;;[:body] (html/append (html/html [:script (browser-connected-repl-js)]))
   [:body :div.navbar :input] (html/set-attr :style "color: red")
   [:body :div.navbar :input.username] (html/set-attr :placeholder "Re-enter Email Address")
   [:body :div.navbar :input.password] (html/set-attr :placeholder "Re-enter Password"))
@@ -124,19 +124,19 @@
   [:body :div.navbar] (html/substitute (navbar req))
   [:body :#content :form :input] (html/set-attr :class "input-block-level btn-lg register alert-danger")
   [:body :div.navbar :ul [:li html/first-of-type]] (html/set-attr :class "active")
-  [:body] (html/append (html/html [:script (browser-connected-repl-js)])))
+  ;;[:body] (html/append (html/html [:script (browser-connected-repl-js)]))
+  )
 
 ;;; App page
 (html/deftemplate welcome (io/resource "public/welcome.html")
   [req]
   [:body :div.navbar] (html/substitute (navbar req))
-  [:body] (html/append
-           (html/html [:script (browser-connected-repl-js)]))
+  ;; [:body] (html/append          (html/html [:script (browser-connected-repl-js)]))
   [#{:span.user}] (html/content (trim-email-address (get-friend-username req) )))
 
 ;;; Logging/Debugging
 (defn log-request [req]
-  (println ">>>>" req)) 
+  (println ">>>>" req))
 
 (defn wrap-verbose [h]
   (fn [req]
@@ -150,36 +150,39 @@
   (GET "/about" req (landing req))
   (GET "/contact" req (landing req))
   (GET "/welcome" req
-    ;(println "welcome req:" req)
-    ;(println "(:user (req :params))" (:username (req :params)))
-    ;(println "user name extraction: " (:authentications (:cemerick.friend/identity (:session req))))
-    (friend/authenticated  (welcome req)))
+                                        ;(println "welcome req:" req)
+                                        ;(println "(:user (req :params))" (:username (req :params)))
+                                        ;(println "user name extraction: " (:authentications (:cemerick.friend/identity (:session req))))
+       (friend/authenticated  (welcome req)))
   (GET "/login" req (login req))
   (GET "/logout" req (friend/logout* (resp/redirect "/")))
   (GET "/reregister" req (reregister req))
   (POST "/register" {{:keys [username password] :as params} :params :as req}
-    (if  (check-registration username password)
-      (let [user (create-user (select-keys params [:username :password]))]        
-        (swap! users #(-> % (assoc (str/lower-case username) user))) ; (println "user is " user)        
-        (friend/merge-authentication (resp/redirect "/welcome") user)) ; (println "register redirect req: " req)
-      (resp/redirect "/reregister") ))  
-  (not-found (landing {:uri  "PageNotFound"}))) 
- 
+        (if  (check-registration username password)
+          (let [user (create-user (select-keys params [:username :password]))]
+            (swap! users #(-> % (assoc (str/lower-case username) user))) ; (println "user is " user)
+            (friend/merge-authentication (resp/redirect "/welcome") user)) ; (println "register redirect req: " req)
+          (resp/redirect "/reregister") ))
+  (not-found (landing {:uri  "PageNotFound"})))
+
+
+
+
+
 (def secured-site
   (-> unsecured-site
       (friend/authenticate {:allow-anon? true
                             :default-landing-uri "/welcome"
                             :credential-fn #(creds/bcrypt-credential-fn @users %)
                             :workflows [(workflows/interactive-form)]})
-      ; (wrap-verbose) ; Logging/Debugging
-      ; required Ring middlewares
+                                        ; (wrap-verbose) ; Logging/Debugging
+                                        ; required Ring middlewares
       (wrap-keyword-params)
       (wrap-nested-params)
       (wrap-params)
       (wrap-session)
       (wrap-reload)))
 
-(defn -main []
-  (defonce ^:private server
-    (ring.adapter.jetty/run-jetty #'secured-site {:port 8080 :join? false}))
-  server)
+
+
+
